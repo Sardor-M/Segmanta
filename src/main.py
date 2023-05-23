5e0fcf9d401b64a34516bd82cdc1c9d07d7b01ed
Sardor Madaminov <sardor0968@gmail.com>
2023-05-23 18:13:04 +0900

    load img feature: added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 # Global variables
 drawing = False
@@ -22,11 +23,22 @@ def draw_roi(event, x, y, flags, param):
         cv2.rectangle(img, roi_points[0], roi_points[1], (0, 255, 0), 2)
         cv2.imshow('Image', img)
 
+
 ## Label the ROI
 def label_roi():
     global current_label
     label = input("Enter the label for the ROI (e.g., person, car or etc): ")
     current_label = label
+
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        if filename.endswith((".jpg", ".jpeg", ".png")):
+            img = os.path.join(folder, filename)
+            images.append(img)
+    return images
+
 
 ## Annotate the image
 def annotate_image(image_path):
@@ -73,6 +85,21 @@ def save_annotated_images(annotations):
 
 ## Main function 
 def main():
+    print("1. Load images from local directory")
+    print("2. Load images from Google Drive")
+    choice = int(input("Enter your choice (1 or 2): "))
+
+    if choice == 1:
+        folder_path = input("Enter the path to the folder: ")
+        images = load_images_from_folder(folder_path)
+        if len(images) == 0:
+            print("No names found in the specified folder")
+            return 
+        for i, image in enumerate(images):
+            print(f"{i+1}. {image}")
+
+        #  Here starts 
+
     image_path ="./data/nature.jpg" 
     annotations = annotate_image(image_path)
     save_annotated_images(annotations)
